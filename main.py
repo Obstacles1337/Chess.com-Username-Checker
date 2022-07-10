288ad881099beab44297081e3f470237a6682823
Obby <82127699+Obstacles1337@users.noreply.github.com>
2022-07-09 18:16:31 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+#Commented out the taken, but if you want to view the taken. You can.
+
+import requests
+from colorama import Fore, init
+
+init()
+
+class ChessChecker():
+    def __init__(self):
+
+        self.targets = open("targets.txt").read().splitlines()
+        self.valid = open("valid.txt", "a+")
+    
+    def check(self):
+        for targets in self.targets:
+            checkusername = requests.get(f"https://www.chess.com/callback/user/valid?username={targets}")
+
+            if ("Username is valid" in checkusername.text):
+                print(f"[{Fore.GREEN}+{Fore.WHITE}] {targets} is not taken!")
+                self.valid.write(f"{targets}\n")
+            #elif ("That username is taken." in checkusername.text):
+              #  print(f"[{Fore.RED}+{Fore.WHITE}] {targets} is taken!")
+
+if __name__ == "__main__":
+    Chess = ChessChecker()
+    Chess.check()
